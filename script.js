fb61575c12414c25d98b1a165ea045042fbd2ed6
ankitaras <73051735+ankitaras@users.noreply.github.com>
2020-10-19 13:50:08 +0530

    UPDATE

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function aditya() {
 
   // If the checkbox is checked, display the output text
  if (a.checked == true) {
-    b.style.backgroundImage = "url(https://wallpapersprinted.com/download/2/early_morning_in_bulgaria-wallpaper-1280x800.jpg)";
+    b.style.background="BLACK";
+   
   } else {
-    b.style.backgroundImage = "url(https://images.unsplash.com/photo-1518818419601-72c8673f5852?ixlib=rb-1.2.1&w=1000&q=80)";;
+    b.style.background="WHITE";
   }
 }
